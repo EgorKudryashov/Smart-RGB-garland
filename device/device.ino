7e80cd422ad08f5b4d62ffaa05c9ca35bb65ddf4
EgorKudryashov <70726535+EgorKudryashov@users.noreply.github.com>
2023-02-22 00:22:27 +0300

    Update device

--- device/device.ino
+++ device/device.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h> //Использование модуля для подключения к wi-fi сети
 #include <ESP8266WebServer.h> //Для создания http-сервера
 #include <Adafruit_NeoPixel.h> //Управление светодиодами WS2812
-#include "ArduinoJson.h"
+#include <ArduinoJson.h> //Работа с JSON. Version 5.13.5
 
 #define PIN D1 //Пин управления гирляндой
 
@@ -16,12 +16,24 @@ ESP8266WebServer server(80);
 const char* device_name = "smart RGB-matrix";
 const int lampsNumber = 64;
 
+struct Lamp {
+  int id;
+  int r;
+  int g;
+  int b;
+};
+
+Lamp garland[lampsNumber];
+int garlandBright = 10;
+
+int garlandMode = -1;
+int totalModes = 4;
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(lampsNumber, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
 
   Serial.begin(115200);
-  
   Serial.println("Connecting to ");
   Serial.println(ssid);
   //Подключение к Wi-Fi сети
@@ -38,9 +50,11 @@ void setup() {
   delay(10);
 
   /* URLs для обращения к серверу */
-  server.on("/connection", handle_OnConnect);
+  server.on("/connection", onConnection);
+  server.on("/refreshGarland", refreshAllLamps);
   server.on("/changeColor/", changeLampColor);
   server.on("/setBrightness/", setLampsBrightness);
+  server.on("/chooseMode/", chooseMode);
 
   /* Запуск сервера */
   server.begin();
@@ -56,6 +70,19 @@ void setup() {
 
 void loop() {
   server.handleClient(); //Обработка HTTP-запросов
+
+  if (garlandMode == 0){
+    loadColorsOnGarland();
+  }
+
+  // garlandMode == 1 - RandomColors
+  if (garlandMode == 2){
+    GarlandBlink(500);
+  }
+  if (garlandMode == 3){
+    GarlandFade(200);
+  }
+
 }
 
 void defaultLamps (){
@@ -68,13 +95,33 @@ void defaultLamps (){
   strip.show();
 }
 
-void handle_OnConnect(){
+void loadColorsOnGarland (){
+  for (int i=0; i<lampsNumber; ++i){
+    strip.setPixelColor(i, garland[i].r, garland[i].g, garland[i].b);    
+  }
+  strip.setBrightness(garlandBright);
+  strip.show();
+
+  garlandMode = -1;
+}
+
+void onConnection(){
+  /*Собираем строку в формате JSON*/
+  String res = "{\"name\":";
+  res += device_name;
+  res+= ",\"lampsNumber\":";
+  res+= lampsNumber;
+  res+='}';
+  //Serial.println(res);
+  server.send(200, "application/json",res);
+}
+
+void refreshAllLamps(){
 
   DynamicJsonBuffer jsonBuffer(1000);
   JsonObject& root = jsonBuffer.parseObject(server.arg("plain"));
-  Serial.println(root["lamps"][0]["r"].as<int>());
   Serial.println("New connection");
-  String message = "Number of args received:";
+/*  String message = "Number of args received:";
   message += server.args();      // получить количество параметров
   message += "\n";               // переход на новую строку
 
@@ -85,6 +132,15 @@ void handle_OnConnect(){
     message += server.arg(i) + "\n";          // получить значение параметра
   } 
   Serial.println(message);
+  */
+  for (int i=0; i<lampsNumber; ++i){
+    garland[i].r = root["lamps"][i]["r"];
+    garland[i].g = root["lamps"][i]["g"];
+    garland[i].b = root["lamps"][i]["b"];
+  }
+
+  loadColorsOnGarland();
+
   server.send(200);
 }
 
@@ -105,10 +161,11 @@ void setLampsBrightness (){
   }
 
   if (isTrue){
-    strip.show();
     strip.setBrightness(bright);
-    server.send(200);
     strip.show();
+    garlandBright = bright;
+
+    server.send(200);
   }
   else {
     server.send (404, "text/plain", "Error 404");
@@ -172,6 +229,9 @@ void changeLampColor(){
 
   if (change){
     strip.setPixelColor(id, r,g,b);
+    garland[id].r = r;
+    garland[id].g = g;
+    garland[id].b = b;
     strip.show();
 
     server.send(200);
@@ -182,4 +242,73 @@ void changeLampColor(){
   }
 
 
+}
+
+void chooseMode (){
+  bool isTrue = true;
+  int mode;
+
+  if (server.arg("mode")== "") 
+  { // параметр не найден
+    Serial.print("Ошибка в передаче запроса, нет аргумента mode");
+    isTrue = false;
+  }
+  else
+  { // параметр найден
+    mode = (server.arg("mode")).toInt();
+    Serial.println(mode);
+    if (mode>=totalModes) {isTrue = false;}
+  }
+  server.send(200);
+
+  if (mode == 0){
+    garlandMode = 0;
+  }
+
+  if (mode == 1) {
+    RandomColors();
+  }
+  if (mode == 2) {
+    garlandMode = 2;
+  }
+  if (mode == 3) {
+    garlandMode = 3;
+  }
+}
+
+void RandomColors (){
+  for (int i=0; i<lampsNumber; ++i){
+    strip.setPixelColor(i, random(0,255), random(0,255), random(0,255));
+  }
+  strip.show();
+}
+
+void GarlandBlink(int wait){
+  for (int i = 0; i < lampsNumber; ++i){
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+  strip.show();
+  delay(wait);
+  for (int i = 0; i < lampsNumber; ++i)
+  {
+    strip.setPixelColor(i, garland[i].r, garland[i].g, garland[i].b);
+  }
+  strip.show();
+  delay(wait);
+}
+
+void GarlandFade(int FadeSpeed)
+{
+  for (int i = 180; i > 5; i-=10)
+  {
+    strip.setBrightness(i);
+    strip.show();
+    delay(FadeSpeed);
+  }
+  for (int i = 5; i < 180; i+=10)
+  {
+    strip.setBrightness(i);
+    strip.show();
+    delay(FadeSpeed);
+  }
 }
